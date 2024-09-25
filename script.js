1fdc4dff2662be173754f4236f5e8ff37c520817
Denys Lacerda Delfino <denyslacerda1990@gmail.com>
2024-09-25 17:59:58 -0300

    Voltando ao código original

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
 function trocaCor(cor) {
-    document.body.style.backgroundImage = 'none';
-    document.body.style.backgroundColor = cor;
+    document.body.style.backgroundImage = 'none'
+    document.body.style.backgroundColor = cor
 }
 
 function corAleatoria() {
-    const red = Math.floor(Math.random() * 255);
-    const green = Math.floor(Math.random() * 255);
-    const blue = Math.floor(Math.random() * 255);
+    const red = Math.floor(Math.random() * 255)
+    const green = Math.floor(Math.random() * 255)
+    const blue = Math.floor(Math.random() * 255)
 
-    document.body.style.backgroundImage = 'none';
-    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    document.body.style.backgroundImage = 'none'
+
+    document.body.style.backgroundColor = `rgb( ${red}, ${green}, ${blue})`
 }
 
 function aplicaCorPersonalizada() {
-    const corInput = document.querySelector('.input-cor').value;
-    if (isValidColor(corInput)) {
-        trocaCor(corInput);
-    } else {
-        alert('Por favor, insira um valor de cor válido (nome da cor ou código HEX).');
-    }
-}
+    const corInput = document.querySelector('.input-cor').value
 
-function isValidColor(strColor) {
-    const s = new Option().style;
-    s.color = strColor;
-    return s.color !== '';
+    trocaCor(corInput)
 }
 
 function escolherImagem(imagem) {
-    const reader = new FileReader();
+    const reader = new FileReader()
 
     reader.onload = function (evento) {
-        const urlImagem = evento.target.result;
-        document.body.style.backgroundImage = `url(${urlImagem})`;
-    };
+        const urlImagem = evento.target.result
 
-    reader.readAsDataURL(imagem);
+        document.body.style.backgroundImage = `url(${urlImagem})`
+    }
+
+    reader.readAsDataURL(imagem)
+
 }
